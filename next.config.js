bad49f3ad9c9e21e0fb94916730d3c4fa2513073
Mohammed Yaseen A <yaseenyash55@gmail.com>
2026-06-21 13:12:15 +0530

    Update next.config.js to include images configuration for unoptimized images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <-- Indha line dhaan ./out folder-a create pannum
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
